caf8de41d18b11b7abfc148227f82f55c1bcd499
Tanner-Wills <tannerwills1@gmail.com>
2021-08-11 13:58:26 -0400

    Initial commit

---
+++ src/ArrayList.java
@@ -0,0 +1,32 @@
+public class ArrayList {
+    //Instance Variables
+    private final int INITIAL_CAPACITY = 9;
+
+    public static void main(String[] args) {
+
+    }
+
+    // *** ArrayList Methods ***
+    private void addToFront(int n) {
+        //When adding to front, all subsequent elements must shift back one space.
+
+    }
+
+    private void addToBack(int n) {
+
+    }
+
+    private void removeFromFront() {
+        //all subsequent elements must shift forward one space.
+
+    }
+
+    private void removeFromBack() {
+        //last element should be set to null. All unused positions in the backingArray must be set to null.
+
+    }
+
+    private void resize(int size){
+        
+    }
+}
