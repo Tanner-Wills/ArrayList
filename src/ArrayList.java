ddf08894ab28697f53f27e23d115dece3ee13cc4
Tanner-Wills <tannerwills1@gmail.com>
2021-08-12 15:25:03 -0400

    updated helper code +1

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,7 +1,7 @@
 import java.util.NoSuchElementException;
 
 /**
- * Your implementation of an ArrayList.
+ * Implementation of an ArrayList.
  */
 public class ArrayList<T> {
 
@@ -20,7 +20,7 @@ public class ArrayList<T> {
 
     /**
      * This is the constructor that constructs a new ArrayList.
-     *
+     * <p>
      * Recall that Java does not allow for regular generic array creation,
      * so instead we cast an Object[] to a T[] to get the generic typing.
      */
@@ -31,9 +31,9 @@ public class ArrayList<T> {
 
     /**
      * Adds the data to the front of the list.
-     *
+     * <p>
      * This add may require elements to be shifted.
-     *
+     * <p>
      * Method should run in O(n) time.
      *
      * @param data the data to add to the front of the list
@@ -41,11 +41,18 @@ public class ArrayList<T> {
      */
     public void addToFront(T data) {
         // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        //Recursive functions call themselves
+        if (data == null) {
+            throw new IllegalArgumentException("Can't add null to a list!");
+        } else {
+            size += 1;
+            backingArray[size] = data;
+        }
     }
 
     /**
      * Adds the data to the back of the list.
-     *
+     * <p>
      * Method should run in amortized O(1) time.
      *
      * @param data the data to add to the back of the list
@@ -53,61 +60,70 @@ public class ArrayList<T> {
      */
     public void addToBack(T data) {
         // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+        if (data == null) {
+            throw new IllegalArgumentException("Can't add null to a list!");
+        } else {
+            if(size +1 > INITIAL_CAPACITY){
 
-    /**
-     * Removes and returns the first data of the list.
-     *
-     * Do not shrink the backing array.
-     *
-     * This remove may require elements to be shifted.
-     *
-     * Method should run in O(n) time.
-     *
-     * @return the data formerly located at the front of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
-    public T removeFromFront() {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+            }
+            size += 1;
+            backingArray[size] = data;
+        }
 
-    /**
-     * Removes and returns the last data of the list.
-     *
-     * Do not shrink the backing array.
-     *
-     * Method should run in O(1) time.
-     *
-     * @return the data formerly located at the back of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
-    public T removeFromBack() {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+        /**
+         * Removes and returns the first data of the list.
+         *
+         * Do not shrink the backing array.
+         *
+         * This remove may require elements to be shifted.
+         *
+         * Method should run in O(n) time.
+         *
+         * @return the data formerly located at the front of the list
+         * @throws java.util.NoSuchElementException if the list is empty
+         */
+        public T removeFromFront () {
+            // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        }
 
-    /**
-     * Returns the backing array of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the backing array of the list
-     */
-    public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
-        return backingArray;
-    }
+        /**
+         * Removes and returns the last data of the list.
+         *
+         * Do not shrink the backing array.
+         *
+         * Method should run in O(1) time.
+         *
+         * @return the data formerly located at the back of the list
+         * @throws java.util.NoSuchElementException if the list is empty
+         */
+        public T removeFromBack () {
+            // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        }
 
-    /**
-     * Returns the size of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the list
-     */
-    public int size() {
-        // DO NOT MODIFY THIS METHOD!
-        return size;
+        /**
+         * Returns the backing array of the list.
+         *
+         * For grading purposes only. You shouldn't need to use this method since
+         * you have direct access to the variable.
+         *
+         * @return the backing array of the list
+         */
+        public T[] getBackingArray () {
+            // DO NOT MODIFY THIS METHOD!
+            return backingArray;
+        }
+
+        /**
+         * Returns the size of the list.
+         *
+         * For grading purposes only. You shouldn't need to use this method since
+         * you have direct access to the variable.
+         *
+         * @return the size of the list
+         */
+        public int size(){
+            // DO NOT MODIFY THIS METHOD!
+            return size;
+        }
     }
 }
---
+++ src/TestArray.java
@@ -0,0 +1,9 @@
+public class TestArray {
+
+    public static <T> void main(String[] args) {
+        ArrayList<Integer> TestyTime = new ArrayList<>();
+
+        TestyTime.addToBack(1);
+
+    }
+}
